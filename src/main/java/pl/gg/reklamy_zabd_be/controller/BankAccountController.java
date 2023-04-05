c6a8f9534f60d0ae23a007cf19ea349d05fbf8c5
Adam <72575016+adam-glab@users.noreply.github.com>
2023-04-05 13:39:39 +0200

    finito

--- src/main/java/pl/gg/reklamy_zabd_be/controller/BankAccountController.java
+++ src/main/java/pl/gg/reklamy_zabd_be/controller/BankAccountController.java
@@ -32,7 +32,7 @@ public class BankAccountController {
         return "bank_account_list";
     }
 
-    @GetMapping
+    @GetMapping("/form")
     public String bankAccountForm(Model model) {
         model.addAttribute("bankAccount",new BankAccount());
         model.addAttribute("companies",companyService.getAllCompanies());
